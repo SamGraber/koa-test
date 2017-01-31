4f7c2fb0c91639e46a30f1c4d5f242960b4b49dd
SamGraber <authorsamgraber@gmail.com>
2017-01-31 15:27:51 -0500

    Implemented session-based auth with koa-session
    Next we'll try to replace this with jwt tokens

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
+import * as session from 'koa-session';
 import { router } from './routes';
+import { keys }from './config/keys';
 
 export const app = new Koa();
 const port = process.env.npm_package_config_port || 3000;
+app.keys = keys;
 
 app.use(errorHandler());
+app.use(session(app));
+app.use((context, next) => {
+	if ((context as any).session.authenticated || context.request.path == '/login' || context.request.path == '/logout') {
+		return next();
+	}
+	this.status = 401;
+});
 app.use(bodyParser());
 app.use(router.routes())
    .use(router.allowedMethods());
--- routes.ts
+++ routes.ts
@@ -10,4 +10,17 @@ router.post('/', context => {
 	console.log(context.request.body);
 	context.body = 'Nice!';
 });
+router.get('/login', context => context.body = 'login');
+router.get('/logout', context => {
+	(context as any).session.authenticated = false;
+	context.redirect('/login');
+});
+router.post('/login', context => {
+	const params = context.request.body;
+	if (params.username !== 'username' || params.password !== 'password') {
+		return this.status = 400;
+	}
+	(context as any).session.authenticated = true;
+	context.redirect('/');
+});
 router.get('*', context => context.body = 'Hello World');
