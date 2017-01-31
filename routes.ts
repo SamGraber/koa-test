7eeba7d1c8e80dc840ab0c97e805093a90eb4ec7
SamGraber <authorsamgraber@gmail.com>
2017-01-31 14:38:53 -0500

    Added a sample api endpoint with json response

--- routes.ts
+++ routes.ts
@@ -4,6 +4,7 @@ import * as Router from 'koa-router';
 export const router = new Router();
 
 router.get('/', context => context.body = 'Dude!');
+router.get('/api/test', context => context.body = { message: 'json message' });
 router.post('/', context => {
 	console.log(context.request.body);
 	context.body = 'Nice!';
