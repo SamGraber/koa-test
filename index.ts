6614bccbbc94b6e28d59580801a23a5ae39e0a65
SamGraber <authorsamgraber@gmail.com>
2017-01-31 14:39:06 -0500

    Added a sample error handler

--- index.ts
+++ index.ts
@@ -5,8 +5,20 @@ import { router } from './routes';
 export const app = new Koa();
 const port = process.env.npm_package_config_port || 3000;
 
+app.use(errorHandler());
 app.use(bodyParser());
 app.use(router.routes())
    .use(router.allowedMethods());
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+function errorHandler() {
+	return async (context, next) => {
+		try {
+			await next();
+		} catch (error) {
+			console.log(error.message);
+			context.body = 'Error';
+		}
+	};
+}
