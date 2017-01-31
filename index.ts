495897cfbdd9ec61f5ef53014f36ff622cb9ee08
SamGraber <authorsamgraber@gmail.com>
2017-01-31 14:25:15 -0500

    Added a body parser

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as Koa from 'koa';
+import * as bodyParser from 'koa-bodyparser';
 import * as Router from 'koa-router';
 
 export const app = new Koa();
@@ -6,8 +7,13 @@ const port = process.env.npm_package_config_port || 3000;
 const router = new Router();
 
 router.get('/', context => context.body = 'Dude!');
+router.post('/', context => {
+	console.log(context.request.body);
+	context.body = 'Nice!';
+});
 router.get('*', context => context.body = 'Hello World');
 
+app.use(bodyParser());
 app.use(router.routes())
    .use(router.allowedMethods());
 
